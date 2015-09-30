99d5348ed3438d05871c3e45bfd529b5ffa9b1c1
Stuart Corbishley <corbish@gmail.com>
2015-09-30 11:00:04 +0200

    Return JoltExceptions in 400 response

--- lib/service.rb
+++ lib/service.rb
@@ -1,16 +1,23 @@
 require './lib/shifter'
 
-before do
+post '/shift' do
   request.body.rewind
   @request_payload = JrJackson::Json.load request.body.read
-end
 
-post '/shift' do
   specs = @request_payload['specs']
   input = @request_payload['input']
 
-  chainr = Chainr.fromSpec(specs)
-  result = chainr.transform(input)
+  begin
+    chainr = Chainr.fromSpec(specs)
+    result = chainr.transform(input)
+  rescue Java::ComBazaarvoiceJoltException::SpecException => e
+    puts e.inspect
+    return [
+      400,
+      {'Content-Type' => 'application/json'},
+      JrJackson::Json.dump({ error: e.message })
+    ]
+  end
   
   [200, {'Content-Type' => 'application/json'}, JrJackson::Json.dump(result)]
 end
